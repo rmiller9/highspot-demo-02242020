ee6d03b2bae44a6a5010a9b9a6444cafde8acae1
RMiller <30965515+rmiller9@users.noreply.github.com>
2020-02-25 13:38:22 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Highspot Test</title>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
